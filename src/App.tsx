334b222fa1a30761e937cb8f50db2004ca50efe5
Richard Suarez <richardmsc24@gmail.com>
2024-12-14 18:27:41 +0100

    refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import { HangImage } from "./components/HangImage";
 import { LettersBoard } from "./components/LettersBoard";
+import { useHangmanGame } from "./hooks/useHangmanGame";
 
 function App() {
-  const [attempts, setAttempts] = useState(0);
-  const [word] = useState("COMPUTADORA");
-  const [hiddenWord, setHiddenWord] = useState("_ ".repeat(word.length));
-  const [lose, setLose] = useState(false);
-  const [won, setWon] = useState(false);
-
-  useEffect(() => {
-    if (attempts >= 9) {
-      setLose(true);
-    }
-  }, [attempts]);
-
-  useEffect(() => {
-    const currentHiddenWord = hiddenWord.split(" ").join("");
-    if (currentHiddenWord === word) {
-      setWon(true);
-    }
-  }, [hiddenWord, word]);
-
-  const checkLetter = (letter: string) => {
-    if (lose) return;
-
-    if (!word.includes(letter)) {
-      //si la letra no existe, suma el intento
-      setAttempts(Math.min(attempts + 1, 9));
-      return;
-    }
-
-    const hiddenWordArray = hiddenWord.split(" ");
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] === letter) {
-        hiddenWordArray[i] = letter;
-      }
-    }
-
-    setHiddenWord(hiddenWordArray.join(" "));
-  };
-
-  const restartGame = () => {
-    setAttempts(0);
-    setHiddenWord("_ ".repeat(word.length));
-    setLose(false);
-    setWon(false);
-  };
+  const { restartGame, checkLetter, attempts, lose, won, hiddenWord, word } =
+    useHangmanGame();
 
   return (
     <>
@@ -55,7 +13,7 @@ function App() {
         <HangImage imageNumber={attempts} />
         <h3>{hiddenWord}</h3>
         <h3>Intentos: {attempts}</h3>
-        {lose ? <h3>Ha Perdido</h3> : ""}
+        {lose ? <h3>Ha Perdido {word}</h3> : ""}
         {won ? <h3>¡Ha Ganado!</h3> : ""}
         <button onClick={restartGame}>Restart</button>
         <LettersBoard checkLetter={checkLetter} />
---
+++ src/hooks/useHangmanGame.ts
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { getRandomWord } from "../helpers/getRandomWord";
+
+export function useHangmanGame() {
+  const [attempts, setAttempts] = useState(0);
+  const [word, setWord] = useState(getRandomWord());
+  const [hiddenWord, setHiddenWord] = useState("_ ".repeat(word.length));
+  const [lose, setLose] = useState(false);
+  const [won, setWon] = useState(false);
+
+  useEffect(() => {
+    if (attempts >= 9) {
+      setLose(true);
+    }
+    const currentHiddenWord = hiddenWord.split(" ").join("");
+
+    if (currentHiddenWord === word) {
+      setWon(true);
+    }
+  }, [attempts, hiddenWord, word]);
+
+  const checkLetter = (letter: string) => {
+    if (lose || won) return;
+
+    if (!word.includes(letter)) {
+      setAttempts(Math.min(attempts + 1, 9));
+      return;
+    }
+
+    const hiddenWordArray = hiddenWord.split(" ");
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === letter) {
+        hiddenWordArray[i] = letter;
+      }
+    }
+
+    setHiddenWord(hiddenWordArray.join(" "));
+  };
+
+  const restartGame = () => {
+    const newWord = getRandomWord();
+    setWord(newWord);
+    setAttempts(0);
+    setHiddenWord("_ ".repeat(newWord.length));
+    setLose(false);
+    setWon(false);
+  };
+
+  return { restartGame, checkLetter, attempts, hiddenWord, word, lose, won };
+}
