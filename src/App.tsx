5dce1b2b589828db42c13ab0fe90729a0a28223a
Richard Suarez <richardmsc24@gmail.com>
2024-12-15 12:12:38 +0100

    fixing styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./styles/App.css";
+import "./styles/buttons.css";
 import { HangImage } from "./components/HangImage";
 import { LettersBoard } from "./components/LettersBoard";
 import { useHangmanGame } from "./hooks/useHangmanGame";
@@ -10,13 +11,26 @@ function App() {
   return (
     <>
       <div className="App">
-        <HangImage imageNumber={attempts} />
-        <h3>{hiddenWord}</h3>
-        <h3>Intentos: {attempts}</h3>
-        {lose ? <h3 className="text-red-50">Ha Perdido {word}</h3> : ""}
-        {won ? <h3 className="text-blue">¡Ha Ganado!</h3> : ""}
-        <button onClick={restartGame}>Restart</button>
-        <LettersBoard checkLetter={checkLetter} />
+        <div className="left-side border-content">
+          <div className="left-side-content ">
+            <div>
+              <HangImage imageNumber={attempts} />
+            </div>
+            <div>
+              <h3>{hiddenWord}</h3>
+            </div>
+          </div>
+          <div className="left-side-letterBoard">
+            <LettersBoard checkLetter={checkLetter} />
+          </div>
+        </div>
+        <div className="right-side border-content">
+          <button onClick={restartGame}>Restart</button>
+
+          <h3>Intentos: {attempts}</h3>
+          {lose ? <h3 className="text-red-50">Ha Perdido {word}</h3> : ""}
+          {won ? <h3 className="text-blue">¡Ha Ganado!</h3> : ""}
+        </div>
       </div>
     </>
   );
